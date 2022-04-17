b5d432fe65ff5e975b2391b02884264fc2282830
ElnatanS <elnatansm2021@gmail.com>
2022-04-17 21:45:24 +0300

    commit first try

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main - prints alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for(i = 'a'; i <= 'z';i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+		printf("%d", ch);
+	printf("\n");
+
+	return (0);
+}
